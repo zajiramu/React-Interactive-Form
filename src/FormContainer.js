e9dce0d0ef5fda0c750e1127927f9c3b79e68cc5
umari <umarijaz3014@gmail.com>
2021-11-26 15:32:33 -0800

    Update FormContainer.js
    add empty method handleChange() to FormContainer class

--- src/FormContainer.js
+++ src/FormContainer.js
@@ -5,8 +5,13 @@ class FormContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+
         };
+        
+    }
+
+    handleChange() {
+
     }
 
     render() {
