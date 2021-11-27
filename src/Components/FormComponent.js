a41a068dcfc2bd19e23c4634b33c6df5a991ae0d
umari <umarijaz3014@gmail.com>
2021-11-26 16:24:28 -0800

    Update FormComponent.js
    add 'value' 'name' & 'onChange' attributes to text input element labeled 'Name:'

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -6,7 +6,7 @@ function FormComponent(props) {
             <form>
                 <fieldset> 
                     <legend>Basic Info</legend>
-                    <label>Name: <input type="text"></input></label> <br/>
+                    <label>Name: <input type="text" name="name" value={props.state.name} onChange={props.handleChange}></input></label> <br/>
                     <label>Email: <input type="text"></input></label> <br/>
                     <label>Job Role:</label>
                     <select>
