b5f037f50d31894091e2ecbd52f5d6ffca60446f
umari <umarijaz3014@gmail.com>
2021-11-26 16:26:24 -0800

    Update FormComponent.js
    remove second fieldset containing T-Shirt info as well as submit button

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -18,34 +18,6 @@ function FormComponent(props) {
                         <option>Other</option>
                     </select>
                 </fieldset> 
-
-                <fieldset> 
-                    <legend>T-Shirt Info</legend>
-
-                    <label>Size: </label>
-                    <select>
-                        <option>S</option>
-                        <option>M</option>
-                        <option>L</option>
-                        <option>XL</option>
-                    </select>
-
-                    <label> Design: </label>
-                    <select>
-                        <option>Select Theme</option>
-                        <option>Theme - JS Puns</option>
-                        <option>Theme - I Love JS</option>
-                    </select>
-
-                    <label> Color: </label>
-                    <select>
-                        <option>Cornflower Blue (JS Puns shirt only)</option>
-                        <option>Dark Slate Grey (JS Puns shirt only)</option>
-                        <option>Gold (JS Puns shirt only)</option>
-                    </select>
-                </fieldset>
-                
-                <button>SUBMIT</button>
             </form>
 
         </div>
