3d31ec9a7198a6300ebeab540ab176118ef156f7
umari <61895531+zajiramu@users.noreply.github.com>
2021-11-30 18:41:43 -0800

    Update FormComponent.js
    add conditional rendering of jsx div for holding the error message for first (name) input text element.

--- src/Components/FormComponent.js
+++ src/Components/FormComponent.js
@@ -1,15 +1,18 @@
 import React from "react"
 
 function FormComponent(props) {
+    // logic to handle conditional rendering of error messages for input text elements
+    const nameErrorDiv = props.state.hasName ? <div></div> : <div>{"*Please Enter A Name*"}</div>;
+    const nameInputClassName = props.state.hasName ? "" : "error-message";
     return (
         <div>
             <form>
                 <fieldset> 
                     <legend>Basic Info</legend>
                     <label>Name: </label>
-                    <div></div>
+                    {nameErrorDiv}
                     <input type="text" name="name" value={props.state.name} onChange={props.handleChange} 
-                           onFocus={props.handleFocus} onBlur={props.handleBlur}></input> <br/>
+                           onFocus={props.handleFocus} onBlur={props.handleBlur} className={nameInputClassName}></input> <br/>
                     <label>Email:</label> 
                     <input type="text" name="email" value={props.state.email} onChange={props.handleChange} 
                            onFocus={props.handleFocus} onBlur={props.handleBlur}></input> <br/>
@@ -24,7 +27,6 @@ function FormComponent(props) {
                     </select>
                 </fieldset> 
             </form>
-
         </div>
     )
 }
